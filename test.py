004072112980c88ea3eba707a0e1cc18c0b9e945
peterw23109 <23109@burnside.school.nz>
2026-02-11 12:21:30 +1300

    added database to code

--- test.py
+++ test.py
@@ -1,6 +1,22 @@
-from flask import Flask
+from flask import Flask, g
+import sqlite3
+
+database = 'database.db'
 
 app = Flask(__name__)
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
 @app.route('/')
 def home():
     return "Hello"
