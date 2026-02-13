45d54e52aac2b3fc201653ddd1627b7596e8bb21
peterw23109 <23109@burnside.school.nz>
2026-02-13 13:40:47 +1300

    added a route

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from flask import Flask, g
 import sqlite3
 
-database = 'database.db'
+DATABASE = 'database.db'
 
 app = Flask(__name__)
 
@@ -16,10 +16,28 @@ def close_connection(exception):
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
+        
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
 
 @app.route('/')
 def home():
-    return "Hello"
+    
+    sql = "SELECT * FROM Pokemon;"
+    result = query_db(sql)
+    return str(result)
+
+@app.route("/Pokemon/<int:pokemon_num>")
+def pokemon(pokemon_num):
+    sql = """SELECT * FROM Pokemon 
+         WHERE POkemon_num = ?;"""
+    result = query_db(sql,(pokemon_num,),True)
+    return str(result)
+    
+
 
 if __name__ == "__main__":
     app.run(debug=True) 
